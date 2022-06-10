32973542f085cb1f5ca082070faad33676ba7a8f
Priya Dutta <Priya.Dutta@uk.ey.com>
2022-06-10 16:33:41 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camri',
+    year: 2000
+  };
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
 }
